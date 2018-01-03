bb11e17d1f6a0a8099bab6deeba87272c9a5aff7
Jeffrey Horn <jeffreyh@zillowgroup.com>
2018-01-03 10:03:45 -0800

    Update tests to jest

--- test/axiosTest.js
+++ test/axiosTest.js
@@ -4,39 +4,39 @@ const express = require('express');
 const axiosTest = require('../');
 
 describe('axiosTest', () => {
-  it('takes a function', () => {
+  test('takes a function', () => {
     return axiosTest(function (req, res) {
       res.writeHead(200, { 'Content-Type': 'text/plain' });
       res.end('hello world');
     })
     .get('/')
     .then((res) => {
-      expect(res.status).to.eql(200);
-      expect(res.data).to.eql('hello world');
+      expect(res.status).toEqual(200);
+      expect(res.data).toEqual('hello world');
     });
   });
 
-  it('takes an app', () => {
+  test('takes an app', () => {
     const app = express();
     app.get('/hello', (req, res) =>  res.send('Hello World!'));
 
     return axiosTest(app.listen())
       .get('/hello')
       .then((res) => {
-        expect(res.status).to.eql(200);
-        expect(res.data).to.eql('Hello World!');
+        expect(res.status).toEqual(200);
+        expect(res.data).toEqual('Hello World!');
       });
   });
 
-  it('takes an app starts calls `listen` if not already listening', () => {
+  test('takes an app starts calls `listen` if not already listening', () => {
     const app = express();
     app.get('/listen', (req, res) =>  res.send('listen'));
 
     return axiosTest(app)
       .get('/listen')
       .then((res) => {
-        expect(res.status).to.eql(200);
-        expect(res.data).to.eql('listen');
+        expect(res.status).toEqual(200);
+        expect(res.data).toEqual('listen');
       });
   });
 });
